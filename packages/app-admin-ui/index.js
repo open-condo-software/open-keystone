6afc34ede114089d2acd20c02b562461556753de
SavelevMatthew <savelevmatthew@gmail.com>
2025-05-23 14:38:25 +0500

    feat(app-admin-ui): add ability to hide counts from dashboard

--- packages/app-admin-ui/index.js
+++ packages/app-admin-ui/index.js
@@ -24,6 +24,7 @@ class AdminUIApp {
     adminMeta = {},
     defaultPageSize = 50,
     maximumPageSize = 1000,
+    showDashboardCounts = true,
   } = {}) {
     if (adminPath === '/') {
       throw new Error("Admin path cannot be the root path. Try; '/admin'");
@@ -52,6 +53,7 @@ class AdminUIApp {
     this._isAccessAllowed = isAccessAllowed;
     this._schemaName = schemaName;
     this._adminMeta = adminMeta;
+    this.showDashboardCounts = showDashboardCounts;
     this.routes = {
       signinPath: `${this.adminPath}/signin`,
       signoutPath: `${this.adminPath}/signout`,
@@ -111,7 +113,7 @@ class AdminUIApp {
 
   getAdminUIMeta(keystone) {
     // This is exposed as the global `KEYSTONE_ADMIN_META` in the client.
-    const { name, adminPath, apiPath, graphiqlPath, pages, hooks } = this;
+    const { name, adminPath, apiPath, graphiqlPath, pages, hooks, showDashboardCounts } = this;
     const { signinPath, signoutPath } = this.routes;
     const { lists } = keystone.getAdminMeta({ schemaName: this._schemaName });
     const authStrategy = this.authStrategy ? this.authStrategy.getAdminMeta() : undefined;
@@ -149,6 +151,7 @@ class AdminUIApp {
       authStrategy,
       lists,
       name,
+      showDashboardCounts,
       ...this._adminMeta,
     };
   }
