fb38977e4248c749ee24fec5c73101f51efae932
Matthew <50069872+SavelevMatthew@users.noreply.github.com>
2025-06-02 12:58:18 +0500

    ci: add publishing job to "@open-keystone" packages (#17)
    * ci: add publishing job to "@open-keystone" packages
    
    * chore: rebase
    
    * fix: add publishConfig to public packages

---
+++ .github/publish-packages.js
@@ -0,0 +1,55 @@
+const commitAnalyzer = require('@mono-pub/commit-analyzer');
+const git = require('@mono-pub/git');
+const github = require('@mono-pub/github');
+const npm = require('@mono-pub/npm');
+const execa = require('execa');
+const publish = require('mono-pub');
+
+/** @type {import('mono-pub').MonoPubPlugin} */
+const builder = {
+  name: '@open-keystone/preconstruct-builder',
+  async prepareAll({ foundPackages }, ctx) {
+    await execa('yarn', ['build'], { cwd: ctx.cwd });
+  },
+};
+
+function getReleaseSection(types, section) {
+  return types.map(type => ({ type, section }));
+}
+
+const BREAKING_KEYWORDS = [
+  'BREAKING CHANGE',
+  'BREAKING CHANGES',
+  'BREAKING-CHANGE',
+  'BREAKING-CHANGES',
+];
+
+// NOTE: Edit this list to add / remove packages from auto-release cycle
+const RELEASE_LIST = ['packages/*'];
+
+publish(RELEASE_LIST, [
+  git(),
+  github({
+    extractCommitsFromSquashed: true,
+    releaseNotesOptions: {
+      rules: [
+        { breaking: true, section: '⚠️ BREAKING CHANGES' },
+        { type: 'feat', section: '✨ New Features' },
+        ...getReleaseSection(['fix', 'hotfix'], '🐛 Bug Fixes'),
+        { type: 'perf', section: '🚀 Performance Improvements' },
+        ...getReleaseSection(
+          ['docs', 'style', 'refactor', 'test', 'build', 'ci', 'chore', 'revert'],
+          '🦖 Other Changes'
+        ),
+        { dependency: true, section: '🌐 Dependencies' },
+      ],
+      breakingNoteKeywords: BREAKING_KEYWORDS,
+    },
+  }),
+  commitAnalyzer({
+    patchTypes: ['fix', 'hotfix', 'revert', 'perf'],
+    breakingNoteKeywords: BREAKING_KEYWORDS,
+  }),
+  builder,
+  npm({ provenance: true }),
+]);
