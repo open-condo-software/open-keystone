6593882f77bae18b0b24bd14ff153828625893d6
pahaz <pahaz.white@gmail.com>
2026-04-19 16:35:53 +0300

    chore: todo example use sqlite by default

--- examples/todo/index.js
+++ examples/todo/index.js
@@ -1,12 +1,16 @@
 const { Keystone } = require('@open-keystone/keystone');
-const { MongooseAdapter } = require('@open-keystone/adapter-mongoose');
+const { PrismaAdapter } = require('@open-keystone/adapter-prisma');
 const { Text } = require('@open-keystone/fields');
 const { GraphQLApp } = require('@open-keystone/app-graphql');
 const { AdminUIApp } = require('@open-keystone/app-admin-ui');
 const { StaticApp } = require('@open-keystone/app-static');
 
 const keystone = new Keystone({
-  adapter: new MongooseAdapter({ mongoUri: 'mongodb://localhost/todo' }),
+  adapter: new PrismaAdapter({
+    provider: 'sqlite',
+    url: 'file:./todo.db',
+    migrationMode: 'prototype',
+  }),
 });
 
 keystone.createList('Todo', {
